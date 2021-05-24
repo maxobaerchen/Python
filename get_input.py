11a6f1c8eb25996e4464f0a1cfa140d9cf247d12
maxobaerchen <71792292+maxobaerchen@users.noreply.github.com>
2021-05-24 18:23:12 +0200

    Add files via upload

---
+++ get_input.py
@@ -0,0 +1,10 @@
+
+def get_number(text_message, data_type, exception_type=ValueError):
+    while True:
+        user_input = input(text_message)
+        try:
+            num_input = data_type(user_input)
+            return num_input
+        except exception_type:
+            print('This is no valid input')
+            continue
