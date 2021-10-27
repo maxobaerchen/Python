b2a2aac8556363f0eb15d837ecd61f23f1769ab7
maxobaerchen <71792292+maxobaerchen@users.noreply.github.com>
2021-10-27 17:11:24 +0200

    Update YouTube-downloader.py

--- YouTube-downloader.py
+++ YouTube-downloader.py
@@ -1,22 +1,44 @@
+import os
+
+import pytube.exceptions
 from pytube import YouTube
+
+
+def get_bunch_list():
+    bunch_string = input('Please enter the links.. ')
+    raw_bunch = bunch_string.strip().split('https://www.youtube.com')
+    links = []
+    for link in raw_bunch[1:]:
+        link = 'https://www.youtube.com' + link.strip()
+        links.append(link)
+    if len(links) == 0:
+        return False
+    return links
 
 
 def get_link_list():
     links = []
+    bunch = bool('y' in input('Do you want to enter a bulk of links? '))
+    if bunch:
+        links = get_bunch_list()
+        if not links:
+            return False
+        return links
+
     while True:
         link = input('Enter the link of the Youtube video you want to download.. ')
         if link == '':
             break
         links.append(link)
-    links = list(set(links))    # removing duplicate links
+
+    links = list(set(links))  # removing duplicate links
     return links
 
 
 def get_path():
     file_path = input('Enter the path where you want to save the download.. ')
     if file_path == '':
-        file_path = '/home/maxo/Storage/Music/'
-    return file_path
+        return False
 
 
 def extract_audio():
@@ -27,9 +49,19 @@ def extract_audio():
 
 
 def get_name(link):
-    video = YouTube(link)
-    name = list(video.title)
-    name.remove('.')
+    try:
+        video = YouTube(link)
+    except pytube.exceptions.RegexMatchError:
+        return False
+    try:
+        name = list(video.title)
+    except pytube.exceptions.VideoUnavailable:
+        return False
+    while True:
+        try:
+            name.remove('.')
+        except ValueError:
+            break
     name = ''.join(name)
     return name
 
@@ -45,30 +77,59 @@ def download(extract: bool, url, path):
 
 
 def convert_mp4_to_mp3(path_to_file, name):
-    os.rename(f'{path_to_file}{name}.mp4', f'{path_to_file}{name}.mp3')
+    try:
+        os.rename(f'{path_to_file}{name}.mp4', f'{path_to_file}{name}.mp3')
+    except FileNotFoundError:
+        try:
+            os.rename(f'{path_to_file} {name}.mp4', f'{path_to_file}{name}.mp3')
+        except FileNotFoundError:
+            pass
 
 
 def main():
     links = get_link_list()
+    if not links:
+        print('Stop it, get some help')
+        return False
     path = get_path()
     extract = extract_audio()
     length = len(links)
     current = 0
+    failed_links = []
     if extract:
+        if not path:
+            path = '/home/maxo/Storage/Music/'
         print('Downloading audio..')
         for link in links:
-            video_name = get_name(link)
+            audio_name = get_name(link)
+            if not audio_name:
+                failed_links.append(link)
+                continue
             download(extract, link, path)
-            convert_mp4_to_mp3(path, video_name)
+            convert_mp4_to_mp3(path, audio_name)
             current += 1
             print(f'Progress: {current}/{length}')
     else:
+        if not path:
+            path = '/home/maxo/Storage/Videos'
         print('Downloading video..')
         for link in links:
+            video_name = get_name(link)
+            if not video_name:
+                failed_links.append(link)
+                continue
             download(extract, link, path)
             current += 1
             print(f'Progress: {current}/{length}')
-    print('Done!')
+    if len(failed_links) != 0:
+        print('Could not download following objects:')
+        for link in failed_links:
+            if not get_name(link):
+                print(f'link: {link}')
+                continue
+            print(f'"{get_name(link)}" (link: {link})')
+    else:
+        print('Done!')
 
 
 if __name__ == '__main__':
