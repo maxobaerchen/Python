d14ce1113500546db31ed50fbd83748fab663914
maxobaerchen <71792292+maxobaerchen@users.noreply.github.com>
2022-02-08 20:36:34 +0100

    Update tool_collection.py

--- tool_collection.py
+++ tool_collection.py
@@ -1,4 +1,5 @@
-import time
+from cProfile import Profile
+import pstats
 
 
 def get_input(text_message, data_type, exception_type=ValueError):
@@ -10,3 +11,16 @@ def get_input(text_message, data_type, exception_type=ValueError):
         except exception_type:
             print('This is no valid input')
             continue
+
+
+def find_slow(function):
+    with Profile() as pr:
+        function()
+
+    stats = pstats.Stats(pr)
+    stats.sort_stats(pstats.SortKey.TIME)
+    stats.print_stats()
+
+
+if __name__ == '__main__':
+    pass
